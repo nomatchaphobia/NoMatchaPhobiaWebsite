f79b764c12b5a5e74960b4a497795e5b098dac97
nomatchaphobia <nomatchaphobia@gmail.com>
2025-08-15 19:21:51 +1200

    Update shared.js

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,11 +1,11 @@
 // Load navbar
-fetch("../partials/nav.html")
+fetch("partials/nav.html")
     .then(res => res.text())
     .then(data => document.getElementById("navbar").innerHTML = data)
     .catch(err => console.error("Navbar load error:", err));
 
 // Load footer
-fetch("../partials/footer.html")
+fetch("partials/footer.html")
     .then(res => res.text())
     .then(data => document.getElementById("footer").innerHTML = data)
     .catch(err => console.error("Footer load error:", err));

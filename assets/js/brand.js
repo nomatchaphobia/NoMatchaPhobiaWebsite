c80dcda492fc69822f3c1dced24e25ce5ade58b9
nomatchaphobia <nomatchaphobia@gmail.com>
2025-08-20 14:01:31 +1200

    update returnTo

--- assets/js/brand.js
+++ assets/js/brand.js
@@ -8,13 +8,15 @@ function renderBrandProducts(brand) {
     ? products
     : products.filter(p => p.brand === brand);
 
+
+
   container.innerHTML = filteredProducts.length > 0
     ? filteredProducts.map(product => `
         <div class="product-card">
           <img src="../${product.image}" alt="${product.name}">
           <h3>${product.name}</h3>
           <p>${product.price}</p>
-        <a href="../partials/product.html?id=${product.id}&brand=${product.brand}&returnTo=/pages/brand.html?brand=${product.brand}" class="btn">View Details</a>
+        <a href="../partials/product.html?id=${product.id}&brand=${product.brand}&returnTo=../pages/brand.html?brand=${product.brand}" class="btn">View Details</a>
 
           ${product.stock > 0
         ? `<button class="add-to-cart">Add to Cart</button>`

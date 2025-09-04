f5ff32d4cd3103578d172231fa8e32a9e5c63280
nomatchaphobia <nomatchaphobia@gmail.com>
2025-09-04 22:26:48 +1200

    change to hardcode nav bar for simplicity

--- assets/js/products.js
+++ assets/js/products.js
@@ -93,7 +93,7 @@ const products = [
     name: "Goun no Shiro - Kanbayashi Shunsho",
     category: "matcha",
     price: "$56",
-    image: "assets/images/matcha/",
+    image: "assets/images/matcha/gounnoshiro",
     description: "",
     brand: "Kanbayashi Shunsho",
     stock: 1,

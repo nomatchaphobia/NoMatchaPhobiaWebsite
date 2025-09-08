f99f9ccff3b4fbc7edb75b33c9d5f3d837d1b367
nomatchaphobia <nomatchaphobia@gmail.com>
2025-09-08 23:28:39 +1200

    update more sold-out UI

--- assets/js/renderCategory.js
+++ assets/js/renderCategory.js
@@ -8,19 +8,31 @@ function renderProducts(category) {
 
 
   
-  container.innerHTML = filteredProducts.map(product => `
-    <div class="product-card">
-      <img src="../${product.image}" alt="${product.name}">
-      <h3>${product.name}</h3>
-      ${product.weight > 0
-        ? `<p>${product.price} - ${product.weight}g</p>`
-        : `<p>${product.price}</p>`
-      }
-      <a href="../partials/product.html?id=${product.id}&returnTo=${encodeURIComponent(window.location.pathname)}" class="btn">View Details</a>
-      <!--${product.stock > 0
-      ? `<button class="add-to-cart">Add to Cart</button>`
-      : `<button disabled>Sold Out</button>`
-    }-->
-    </div>
-  `).join('');
+  container.innerHTML = filteredProducts.map(product => {
+        if (product.stock <= 0) {
+            return `
+                <div class="product-card sold-out">
+                  <img src="../${product.image}" alt="${product.name}">
+                  <h3>${product.name} (sold out)</h3>
+                  ${product.weight > 0
+                    ? `<p>${product.price} - ${product.weight}g</p>`
+                    : `<p>${product.price}</p>`
+                  }
+                  <a href="../partials/product.html?id=${product.id}&brand=${product.brand}&returnTo=${encodeURIComponent(window.location.pathname)}" class="btn">View Details</a>
+                </div>
+            `;
+        } else {
+            return `
+                <div class="product-card">
+                  <img src="../${product.image}" alt="${product.name}">
+                  <h3>${product.name}</h3>
+                  ${product.weight > 0
+                    ? `<p>${product.price} - ${product.weight}g</p>`
+                    : `<p>${product.price}</p>`
+                  }
+                  <a href="../partials/product.html?id=${product.id}&brand=${product.brand}&returnTo=${encodeURIComponent(window.location.pathname)}" class="btn">View Details</a>
+                </div>
+            `;
+        }
+    }).join('');
 }
